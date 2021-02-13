5a7be6c0d6dfe87208e9e426c016bdd97e416fe3
alex valle <alex.valle@gmail.com>
2021-02-13 15:44:16 -0500

    removed unused endpoints

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const path = require('path');
 const app = express();
+const User = require('./backend/User')
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/api/home', function(req, res) {
-  res.send(`Let's go!`);
-});
-app.get('/api/secret', function(req, res) {
-  res.send('The password is thewell');
-});
+app.post('/api/signin', User.signin)
 
 app.listen(process.env.PORT || 8080);
